303586663e1ad0d5a2cb93e36444b86afd4a9504
shyamgpv <shyamgopalpv@yahoo.co.in>
2021-09-04 17:20:48 +1200

    first commit

---
+++ src/main/java/com/shyam/carsaleservice/CarSaleServiceApplication.java
@@ -0,0 +1,20 @@
+package com.shyam.carsaleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CarSaleServiceApplication {
+
+    @GetMapping("/message")
+    public String message(){
+        return "Successfull";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(CarSaleServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shyam/carsaleservice/CarSaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shyam.carsaleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSaleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
