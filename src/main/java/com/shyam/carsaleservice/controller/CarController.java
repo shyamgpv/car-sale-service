4d4b2baf810a37fd78d6bea1e7fd81b54521c8df
shyamgpv <shyamgopalpv@yahoo.co.in>
2021-09-06 19:24:22 +1200

    Added getListing methods for car entity

--- src/main/java/com/shyam/carsaleservice/controller/CarController.java
+++ src/main/java/com/shyam/carsaleservice/controller/CarController.java
@@ -5,6 +5,9 @@ import com.shyam.carsaleservice.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/car")
 public class CarController {
@@ -24,5 +27,34 @@ public class CarController {
     @GetMapping("/getListing/{carID}")
     public Car getCar(@PathVariable Long carID){
         return carRepository.findCarById(carID);
+    }
+
+    @GetMapping(value="/getListings",params = "make")
+    public List getCarByMake(@RequestParam("make") String make){
+        return carRepository.getCarByMake(make);
+    }
+    @GetMapping(value="/getListings",params = "model")
+    public List getCarByCarModel(@RequestParam("model") String carModel){
+        return carRepository.getCarByCarModel(carModel);
+    }
+    @GetMapping(value="/getListings",params = "registration")
+    public List getCarByRegistration(@RequestParam("registration") String registration){
+        return carRepository.getCarByRegistration(registration);
+    }
+    @GetMapping(value="/getListings",params = "colour")
+    public List getCarByColour(@RequestParam("colour") String colour){
+        return carRepository.getCarByColour(colour);
+    }
+    @GetMapping(value="/getListings",params = "year")
+    public List getCarByYear(@RequestParam("year") Integer year){
+        return carRepository.getCarByYear(year);
+    }
+    @GetMapping(value="/getListings",params = {"fromPrice","toPrice"})
+    public List getCarByPriceRange(@RequestParam("fromPrice") Double fromPrice,@RequestParam("toPrice") Double toPrice){
+        return carRepository.getCarByPriceBetween(fromPrice,toPrice);
+    }
+    @GetMapping(value="/getListings",params = {"fromYear","toYear"})
+    public List getCarByYearRange(@RequestParam("fromYear") Integer fromYear,@RequestParam("toYear") Integer toYear){
+        return carRepository.getCarByYearBetween(fromYear,toYear);
     }
 }
--- src/main/java/com/shyam/carsaleservice/repository/CarRepository.java
+++ src/main/java/com/shyam/carsaleservice/repository/CarRepository.java
@@ -4,7 +4,18 @@ import com.shyam.carsaleservice.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car,Long> {
     Car findCarById(Long id);
+
+     List getCarByCarModel(String carModel);
+    List getCarByMake(String make);
+    List getCarByYear(Integer year);
+    List getCarByYearBetween(Integer startYear,Integer endYear);
+    List getCarByColour(String colour);
+    List getCarByPriceBetween(double startPrice,double endPrice);
+    List getCarByRegistration(String registration);
+
 }
