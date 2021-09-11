0f959420aa3855e11573ada58028f5c0eaca0151
shyamgpv <shyamgopalpv@yahoo.co.in>
2021-09-12 11:42:06 +1200

    Root redirect fixed

--- src/main/java/com/shyam/carsaleservice/configuration/WebConfiguration.java
+++ src/main/java/com/shyam/carsaleservice/configuration/WebConfiguration.java
@@ -9,7 +9,7 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/api/swagger-ui.html");
+        registry.addRedirectViewController("/", "/swagger-ui.html");
     }
 
 }
